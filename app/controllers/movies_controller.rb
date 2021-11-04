51f5f4829b12c47190ae39798b64eb5442ee29eb
Samuel <samlee1097@yahoo.com>
2021-11-04 10:42:15 -0400

    commit tree 01e1537ac9c10b900c6757fd6cc10bc109c5ca0d

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,4 @@
 class MoviesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     movies = Movie.all
@@ -7,8 +6,22 @@ class MoviesController < ApplicationController
   end
 
   def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: movie
+    else
+      render json: { error: "Movie not found" }, status: :not_found
+    end
+  end
+
+  def summary
     movie = Movie.find(params[:id])
-    render json: movie
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
   end
 
   private
